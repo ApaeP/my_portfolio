2752d607ea1d260d6350bc054bf074c0873c04ce
Paul Portier <paulportier@gmail.com>
2020-06-09 17:04:49 +0200

    home

---
+++ app/javascript/components/console_messages.js
@@ -0,0 +1,7 @@
+const logMessagesInConsole = () => {
+  console.log('%c%s',
+              "color: #FFF; font-size: 24px; padding: .5rem; font-family: 'courier'; text-align: center;",
+              "Hello, you're welcome to have a peak at my code 😁");
+};
+
+export { logMessagesInConsole };
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -25,10 +25,9 @@ require("channels")
 // External imports
 import "bootstrap";
 
-// Internal imports, e.g:
-// import { initSelect2 } from '../components/init_select2';
+// Imports from components
+import { logMessagesInConsole } from '../components/console_messages'
 
 document.addEventListener('turbolinks:load', () => {
-  // Call your functions here, e.g:
-  // initSelect2();
+  logMessagesInConsole();
 });
